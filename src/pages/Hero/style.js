71844fe9593c7cb7a13864d1e2c3fc535fb2c898
JiaAnTW <at5lp6andy@gmail.com>
2021-01-28 23:22:27 +0800

    fix: correct Hero container's css

--- src/pages/Hero/style.js
+++ src/pages/Hero/style.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
+import { Container } from '@/css/global';
 
-export const HeroLayout = styled.div`
-    display: flex;
+export const HeroLayout = styled(Container)`
     flex-direction: column;
     align-items: center;
+    padding: 30px;
 `;
